af8391eb39a2db9b6ad6115a1809a31147ee032b
Shiloli-r <ronaldshiloli@gmail.com>
2022-05-10 01:14:07 +0300

    Update and free firebase parking

--- web/urls.py
+++ web/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('update/<slug:slug>', views.occupied, name='occupied'),
+    path('free/<slug:slug>', views.free, name='free')
 ]
--- web/views.py
+++ web/views.py
@@ -1,6 +1,28 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
 from django.shortcuts import render
+cred = credentials.Certificate("D:\Projects\PX hardware\park_xplore\web\serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+
+def occupied(request, slug):
+    parking = db.collection(u'parking_slots').document(slug)
+    parking.update({u'status': "Occupied"})
+    return render(request, 'index.html')
+
+
+def free(request, slug):
+    parking = db.collection(u'parking_slots').document(slug)
+    parking.update({u'status': "free"})
+    return render(request, 'index.html')
+
+
+# 251c5140-c8a3-11ec-9ee0-af43e4fd9d71
+# 979a6d20-c8a2-11ec-9ee0-af43e4fd9d71
+# d4baf510-c476-11ec-9ebd-4f842fec3f71
